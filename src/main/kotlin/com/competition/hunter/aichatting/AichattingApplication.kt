8d50a81c69f2c52cc6326fcc7a680a3e24a463ec
kang jihyeong <her4386@gmail.com>
2024-04-01 14:33:30 +0900

    init

---
+++ src/main/kotlin/com/competition/hunter/aichatting/AichattingApplication.kt
@@ -0,0 +1,11 @@
+package com.competition.hunter.aichatting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AichattingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AichattingApplication>(*args)
+}
---
+++ src/test/kotlin/com/competition/hunter/aichatting/AichattingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.competition.hunter.aichatting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AichattingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
